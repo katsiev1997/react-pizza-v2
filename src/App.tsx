2b90da34d6506c95101db0dfae1a1adfdbd23ff0
katsiev1997 <105316523+katsiev1997@users.noreply.github.com>
2023-09-30 16:38:23 +0300

    Revert "Оптимизируем сборку  Code Splitting,  Reexport"
    This reverts commit aa1c0e978cba1b77fba5456b8b2bc2054e34c497.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,24 @@
-import React from "react";
 import "./scss/app.scss";
-import Loadable from "react-loadable";
 
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import FullPizza from "./pages/FullPizza";
 import MainLayout from "./layouts/MainLayout";
 
-// const Cart = React.lazy(
-//   () => import(/*webpackChunkName: "Cart" */ "./pages/Cart")
-// );
 
-const Cart = Loadable({
-  loader: () => import(/*webpackChunkName: "Cart" */ "./pages/Cart"),
-  loading: () => <div>Идет загрузка корзины...</div>,
-});
-
-const FullPizza = React.lazy(
-  () => import(/*webpackChunkName: "FullPizza" */ "./pages/FullPizza")
-);
-const NotFound = React.lazy(
-  () => import(/*webpackChunkName: "NotFound" */ "./pages/NotFound")
-);
 
 function App() {
   return (
-    <Routes>
-      <React.Suspense fallback={<div>Идет загрузка корзины...</div>}>
+      <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route path="" element={<Home />} />
           <Route path="cart" element={<Cart />} />
           <Route path="*" element={<NotFound />} />
           <Route path="pizza/:id" element={<FullPizza />} />
         </Route>
-      </React.Suspense>
-    </Routes>
+      </Routes>
   );
 }
 
--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Categories } from "../components/Categories";
-export { default as Sort } from "../components/Sort";
-export { default as PizzaBlock } from "../components/PizzaBlock";
-export { default as Skeleton } from "../components/PizzaBlock/Skeleton";
-export { default as Pagination } from "../components/Pagination";
